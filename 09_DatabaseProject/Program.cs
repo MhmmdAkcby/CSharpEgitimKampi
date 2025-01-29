751fd29e1468f1ed23299707b153cdcc36e1b5cb
MhmmdAkcby <muhammedakcabey546@gmail.com>
2025-01-30 00:20:20 +0300

    Reapply "sqk database learn"
    This reverts commit ccba4be188de78cdadbd48ac1ee21d5eb1ba9fbb.

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Adonet
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("-----------------------------------------------------");
+
+            SqlConnection connnection = new SqlConnection("Data Source=LAPTOP-00JKOCIT; initial Catalog=EgitimKampiDb; integrated security=true");
+            connnection.Open();
+            SqlCommand cmd = new SqlCommand("Select * From TblCategory", connnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            foreach(DataRow row in dataTable.Rows)
+            {
+                foreach(var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+            connnection.Close();
+
+            Console.Read();
+        }
+    }
+}
