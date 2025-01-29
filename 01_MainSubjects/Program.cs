0bd37e8b24bd3e6e21380a8f81343f33960eb0d7
MhmmdAkcby <muhammedakcabey546@gmail.com>
2025-01-29 21:54:21 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Yazdırma Komutları
+            //Console.WriteLine("Hello World");
+            //Console.WriteLine("Hi");
+            //Console.WriteLine("1-Çorbalar");
+            //Console.WriteLine("2-Ana Yemekler");
+            //Console.WriteLine("3-Soğuk Başlangıçlar");
+            //Console.WriteLine("4-Salatalar");
+            //Console.WriteLine("5-Tatlılar");
+            //Console.WriteLine("6-İçecekler");
+            //Console.WriteLine();
+            //Console.WriteLine("***** Yemek Kategorileri *****");
+
+            #endregion
+
+
+            #region String Değişkenler
+            //string
+            //Değişken türü değiken adi
+            //string name;
+            //name = "Murat";
+            //Console.WriteLine(name);
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Ali";
+            //customerSurname = "Çınar";
+            //customerPhone = "+90 500 400 30 20";
+            //customerEmail = "deneme@gmail.com";
+            //district = "Kadıköy";
+            //city = "Istanbul";
+
+            //Console.WriteLine("**** Rezervasyon Kartı ****");
+            //Console.WriteLine();
+            //Console.WriteLine("-------------------------------------------");
+            //Console.WriteLine($"Müşteri: {customerName} {customerSurname} ");
+            //Console.WriteLine($"İletişim: {customerPhone}");
+            //Console.WriteLine($"Email: {customerEmail}");
+            //Console.WriteLine($"Adres: {district}/{city}");
+            //Console.WriteLine("-------------------------------------------");
+
+            //customerName = "Ayşegül";
+            //customerSurname = "Kaya";
+            //customerPhone = "+90 400 300 80 70";
+            //customerEmail = "deneme@gmail.com";
+            //district = "Sapanca";
+            //city = "Sakarya";
+            ////Console.WriteLine(customerName);
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("-------------------------------------------");
+            //Console.WriteLine($"Müşteri: {customerName} {customerSurname} ");
+            //Console.WriteLine($"İletişim: {customerPhone}");
+            //Console.WriteLine($"Email: {customerEmail}");
+            //Console.WriteLine($"Adres: {district}/{city}");
+            //Console.WriteLine("-------------------------------------------");
+            #endregion
+
+            #region Int Değişkenler
+                
+            //int number = 24;
+            //Console.WriteLine(number);
+
+            int hamburgerPrice = 300;
+            int cokePrice = 35;
+            int waterPrice = 10;
+            int friesPrice = 50;
+            int pizzaPrice = 250;
+            int lemonadePrice = 30;
+
+            Console.WriteLine("***** Resoran Menu Fiyatları *****");
+            Console.WriteLine();
+
+            Console.WriteLine($"----Hamburger: {hamburgerPrice} TL");
+            Console.WriteLine($"----Kola: {cokePrice} TL");
+            Console.WriteLine($"----Su: {waterPrice} TL");
+            Console.WriteLine($"----Kızartmalar: {friesPrice} TL");
+            Console.WriteLine($"----Pizza: {pizzaPrice} TL");
+            Console.WriteLine($"----Limonata: {lemonadePrice} TL");
+
+            Console.WriteLine();
+            #endregion
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 07_ForeachLoops/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _07_ForeachLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Foreach Döngüsü
+
+            //string[] cities = { "milano", "roma", "budapeşte", "ankara", "istanbul", "varşova" };
+
+            //foreach (string city in cities) Console.WriteLine(city);
+
+            //int[] numbers = {45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+            //foreach ( int i in numbers) Console.WriteLine(i);
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+
+            //foreach( var i in numbers )
+            //{
+            //    if (i % 2 == 0) Console.WriteLine(i);
+            //}
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+
+            //int total = 0;
+
+            //foreach(var i in numbers) total += i;
+
+            //Console.WriteLine(total);
+
+            //List<int> numbers = new List<int>()
+            //{
+            //    1,2,3,4,5
+            //};
+            //foreach (int number in numbers) Console.WriteLine(number);
+
+            //string word = "Merhaba";
+
+            //foreach (char c in word) Console.WriteLine(c);
+
+            #endregion
+
+            #region Örnek Sınav Sistemi Uygulaması
+
+            Console.WriteLine("***** C# Eğitim Kampı Sınava Uygulaması *****");
+
+            Console.WriteLine();
+            Console.Write("Sınıftaki öğrenci sayısını giriniz: ");
+            int studentsCount = int.Parse(Console.ReadLine());
+            string[] student = new string[studentsCount];
+
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                Console.Write($"Lütfen {i}. Öğrenci Adını Giriniz: ");
+                string studentName = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("1. Sınav Notunu Giriniz: ");
+                int exam1 = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write("2. Sınav Notunu Giriniz: ");
+                int exam2 = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                int result = (exam1 + exam2) / 2;
+
+                Console.WriteLine();
+                student[i] = $"Öğrenci Adı: {studentName}  1. Sınavı: {exam1}   2. Sınavı {exam2}  Ortalaması: {result}";
+                
+
+            }
+
+            foreach (var i in student)
+            {
+                Console.WriteLine("**********************************************************************************");
+                Console.WriteLine();
+                Console.WriteLine(i);
+                Console.WriteLine();
+                Console.WriteLine("**********************************************************************************");
+
+            }
+
+
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
